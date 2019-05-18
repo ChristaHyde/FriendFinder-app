4b34ff0163cc12366badd23c7632cb0020e2ecb9
Christa <christahyde@yahoo.com>
2019-05-18 13:44:24 -0600

    Finished

---
+++ app/data/friends.js
@@ -0,0 +1,97 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var friendArray = [
+
+  {
+    "name": "Joey",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Bill",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      6,
+      1,
+      4,
+      4,
+      9,
+      1,
+      2,
+      4,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Andrew",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      3,
+      2,
+      5,
+      2,
+      5,
+      2,
+      3,
+      5,
+      2,
+      4
+    ]
+  },
+  {
+    "name": "John",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      3,
+      4,
+      2,
+      1,
+      2,
+      5,
+      3,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Susan",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      4,
+      3,
+      2,
+      5,
+      1,
+      3,
+      4,
+      2,
+      5,
+      1
+    ]
+  }
+
+];
+
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
+
+
